2343de8f73b151adde87b4fd1c6f153053275549
iasonkoutsoulis <iasonkoutsoulis@gmail.com>
2023-07-21 11:29:46 +0200

    in the end we work with yfinance

---
+++ python code/neu_net.py
@@ -0,0 +1,10 @@
+'''
+with the present we'll train the text data along with the financial data and get our trained net.
+this will require some manipulation of the current state of the data, which makes sense that is done here,
+in order to promote homogeneity and peace of mind...
+'''
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
--- python code/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-import requests
-import re
-from bs4 import BeautifulSoup as bs
-import pandas as pd
