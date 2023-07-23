901626065408ee1cc72a8bffc832966b70ade1cb
iasonkoutsoulis <iasonkoutsoulis@gmail.com>
2023-07-23 15:51:39 +0200

    vectorization complete and now we do the preprocessing, only for one variable just yet

--- python code/Tralgo.py
+++ python code/Tralgo.py
@@ -31,22 +31,29 @@ def tl_collect(all_links, years):
                 text_links.append(link)
     return text_links
 
+#
 # main script 
 bimon_arts = dict()
 for page in range(287, 280, -1):
     print(str(page))
+    
+    #
     # initialize using the front-page links
     url = 'https://www.theguardian.com/business/stock-markets?page=' + str(page) # total pages = 287
     html = requests.get(url).text
     soup = bs(html, 'lxml')
-
+    
+    #
     # get all article links from the page we've opened (we use the year they include to identify them)
+    
     all_links = link_collect(soup)
     years = year_collect(soup)
     text_links = tl_collect(all_links, years) 
-
+    
+    #
     # now we open all of the articles on the page and collect them into our bimonthly datasets
     # we create a dictionary/log entry which holds all text for a span of 15 days.
+    
     for tlink in text_links:
         subhtml = requests.get(tlink).text
         subsoup = bs(subhtml, 'lxml')
@@ -80,7 +87,9 @@ for page in range(287, 280, -1):
         else:
             bimon_arts[art_date + '-' + bimon].append(art_str)
 
+#
 # instead of text files I'll try the JSON stuff now
+
 with open('E:/Tralgo/articles/article_container.json', "w") as f:
     json.dump(bimon_arts, f)
 
--- python code/neu_net.py
+++ python code/neu_net.py
@@ -16,6 +16,7 @@ import torch.optim as optim
 
 #
 # Fetch articles data
+
 with open('E:/Tralgo/articles/article_container.json', 'r') as f:
     articles_text = json.load(f)
 
@@ -27,47 +28,47 @@ flat_articles_list = [item for sublist in articles_text.values() for item in sub
 #
 # we tokenize our text data with their relevant tags from the dictionary
 
-tagged_data = []
+tagged_arts = []
 for period, articles in articles_text.items():
     for article in articles:
         tag = period
-        tagged_data.append(TaggedDocument(words=article.lower().split(), tags=[tag]))
+        tagged_arts.append(TaggedDocument(words=article.lower().split(), tags=[tag]))
 
 #
 # here we train our Doc2Vec model on every word of every article with attention to tags
-doc2vec_model = Doc2Vec(tagged_data, vector_size=300, window=5, min_count=1, workers=4)
 
+doc2vec_model = Doc2Vec(tagged_arts, vector_size=300, min_count=2, epochs=10000)
 
+#
+# next we'll work on the embeddings a bit
+
+doc_embeds = {}
+for period, embeds in articles_text.items():
+    doc_embeds[period] = doc2vec_model.docvecs[period]
+
+doc_embeds_tens = {}
+for period, embeds in doc_embeds.items():
+    doc_embeds_tens[period] = np.array(embeds)
+
+#
 # in this part we transform words into vectors
-vect_dict = dict()
+
 vectorizer = TfidfVectorizer()
-for key in articles_text:
-    articles = articles_text[key]
-    vectors = vectorizer.fit_transform(articles)
+vectors = {period: vectorizer.fit_transform(articles).toarray().astype(np.float32).flatten() for period, articles in articles_text.items()}
 
-    # convert to PyTorch tensors
-    vectors_np = vectors.toarray()
-    vectors_tensor = torch.tensor(vectors_np, dtype=torch.float32)
+#
+# and now we combine document embeddings with TF-IDF vectors for each period's articles
 
-    vect_dict[key] = vectors_tensor
+X_data = {}
+for period in articles_text.keys():
+    embeds_vecs = np.concatenate((vectors[period], doc_embeds_tens[period]), axis=0)
+    X_data[period] = torch.tensor(embeds_vecs, dtype=torch.float32)
 
+X = pd.DataFrame.from_dict(X_data, orient='index')
 
-df_arts = pd.DataFrame.from_dict(vect_dict, orient='index')
-df_fins = feather.read_feather('E:/Tralgo/data/financial_container.csv')
+Y_data = feather.read_feather('E:/Tralgo/data/financial_container.csv')
+Y = Y_data['TSLA_indicator']
 
-dft = df_fins.join(df_arts)
-dft.to_clipboard()
+#
+# now we'll do some preprocessing of our data
 
-# a = torch.ones(3)
-# float(a[1])
-# a[2] = 2.0
-
-# points = torch.tensor([[4.0, 1.0], [5.0, 3.0]]) # tensor takes only one positional argument
-# points.shape
-# points[1, 1]
-
-# img_t = torch.randn(3, 5, 5)
-# weights = torch.tensor([0.2126, 0.7152, 0.0722])
-# batch_t = torch.randn(2, 3, 5, 5) # (batch, channels, rows, columns)
-
-# weights_named = torch.tensor([0.2126, 0.7152, 0.0722], names=['channels'])
