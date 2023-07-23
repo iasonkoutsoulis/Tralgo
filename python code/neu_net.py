eb44c9a50e2bb61b9c5b363ba5e651762347c248
iasonkoutsoulis <iasonkoutsoulis@gmail.com>
2023-07-23 09:33:51 +0200

    now the doc2vec modelling part is done, I feel. next up embedding and finalization of data

--- python code/neu_net.py
+++ python code/neu_net.py
@@ -3,29 +3,53 @@ with the present we'll train the text data along with the financial data and get
 this will require some manipulation of the current state of the data, which makes sense that is done here,
 in order to promote homogeneity and peace of mind...
 '''
-import pyarrow.feather as feather
+from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 import json
 import numpy as np
 import pandas as pd
+import pyarrow.feather as feather
 from sklearn import preprocessing
 from sklearn.feature_extraction.text import TfidfVectorizer
 import torch
 import torch.nn as nn
 import torch.optim as optim
 
-
+#
+# Fetch articles data
 with open('E:/Tralgo/articles/article_container.json', 'r') as f:
     articles_text = json.load(f)
 
+#
+# in this part I worry about getting a word tokenizer going
+
+flat_articles_list = [item for sublist in articles_text.values() for item in sublist]
+
+#
+# we tokenize our text data with their relevant tags from the dictionary
+
+tagged_data = []
+for period, articles in articles_text.items():
+    for article in articles:
+        tag = period
+        tagged_data.append(TaggedDocument(words=article.lower().split(), tags=[tag]))
+
+#
+# here we train our Doc2Vec model on every word of every article with attention to tags
+doc2vec_model = Doc2Vec(tagged_data, vector_size=300, window=5, min_count=1, workers=4)
+
+
+# in this part we transform words into vectors
 vect_dict = dict()
 vectorizer = TfidfVectorizer()
 for key in articles_text:
     articles = articles_text[key]
-    X = vectorizer.fit(articles)
-    vector = X.transform(articles)
+    vectors = vectorizer.fit_transform(articles)
 
-    vect_dict[key] = vector
+    # convert to PyTorch tensors
+    vectors_np = vectors.toarray()
+    vectors_tensor = torch.tensor(vectors_np, dtype=torch.float32)
 
+    vect_dict[key] = vectors_tensor
 
 
 df_arts = pd.DataFrame.from_dict(vect_dict, orient='index')
